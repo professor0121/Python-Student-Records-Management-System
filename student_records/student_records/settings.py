810edd3eff478c96d22020e40adae88fbee660da
Abhishek Kushwaha <141386274+professor0121@users.noreply.github.com>
2025-02-13 23:26:20 +0530

    all good

--- student_records/student_records/settings.py
+++ student_records/student_records/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(48@z$3ylrw)ti_q8p=%q!q9%d8=_b!=cbatpsznk(tmgnlged'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG =False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['your-app-name.onrender.com']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
